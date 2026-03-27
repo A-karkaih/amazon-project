8dc18fe1cfdac62fa38115793cc3d554abef6f03
A-karkaih <akarkaih@gmail.com>
2026-03-27 19:17:01 +0100

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,21 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
 
+// Save cart to Local Storage
+function saveToLocalStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 // Add ToCart
 export function addToCart(productId) {
   let matchingItem;
@@ -26,9 +33,10 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToLocalStorage();
 }
 
-// Delee Item from cart
+// Delete Item from cart
 
 export function removeFromCart(productId) {
   const newCart = [];
@@ -39,4 +47,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToLocalStorage();
 }
